2c49172b10adf50583afa5edf620fdf5b77329e6
Mabel <mabelsanromanf@gmail.com>
2023-07-18 13:46:56 +0200

    subida reto2dia5

---
+++ reto2dia5.js
@@ -0,0 +1,63 @@
+//reto 2
+
+//funcion clasica
+function calculadora(operacion, op1, op2=0) {
+    if (operacion == 'sum'){
+        return op1 + op2
+    }
+    if (operacion == 'subs') {
+        return op1 - op2
+    }
+    if ( operacion == 'mult'){
+        return op1 * op2
+    }
+    if (operacion == 'div') {
+        return op1 / op2
+    }
+}
+
+console.log(calculadora('sum', 23));
+console.log(calculadora('subs', 6, 88));
+console.log(calculadora('mult', 3, 40));
+console.log(calculadora('div', 5));
+
+//funcion arrow
+let calculadoraArrow = (operacion, op1, op2 = 0) => 
+{if (operacion == 'sum'){
+    return op1 + op2
+} 
+if (operacion == 'subs') {
+    return op1 - op2
+}
+if ( operacion == 'mult'){
+    return op1 * op2
+}
+if (operacion == 'div') {
+    return op1 / op2
+}
+}
+
+console.log(calculadoraArrow('sum', 23));
+console.log(calculadoraArrow('subs', 90, 88));
+console.log(calculadoraArrow('mult', 9, 40));
+console.log(calculadoraArrow('div', 5));
+
+//spread operator
+let paramsSum = ['sum', 23];
+let paramsSubs = ['subs', 90, 88];
+let paramsMult = ['mult', 9, 40];
+let paramsDiv = ['div', 5];
+
+console.log(calculadora(...paramsSum));
+console.log(calculadora(...paramsSubs));
+console.log(calculadora(...paramsMult));
+console.log(calculadora(...paramsDiv));
+
+//Añade a este archivo de funciones, una nueva función que haga el cuadrado de un número.
+function cuadradonum(num) {
+    let resultado = num * num;
+    return resultado;
+}
+let num = 8;
+let alcuadrado = cuadradonum(num);
+console.log(alcuadrado);
